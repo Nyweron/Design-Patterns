db820204a2239bd93d270ee153ebcf2a9c8111ea
Przemysław Juśkiewicz <nyweron@gmail.com>
2019-09-24 15:46:20 +0200

    Removed unnecessary code

--- Program.cs
+++ Program.cs
@@ -7,41 +7,7 @@ namespace DesignPatterns
   {
     static void Main(string[] args)
     {
-      var order = new Order(110, OrderStatus.Registered);
 
-      var textMessage = new TextMessage();
-      var mobileApp = new MobileApp();
-      var email = new Email();
-
-      System.Console.WriteLine();
-
-      order.RegisterObserver(textMessage);
-      order.RegisterObserver(mobileApp);
-      order.RegisterObserver(email);
-
-      order.NotifyObservers();
-
-      System.Console.WriteLine();
-
-      order.ChangedOrderStatus(OrderStatus.Sent);
-
-      order.UnregisterObserver(email);
-
-      System.Console.WriteLine();
-
-      order.ChangedOrderStatus(OrderStatus.Received);
-
-      System.Console.WriteLine();
     }
   }
 }
-
-/*
-PROBLEMS:
-1. If we changed order then we need again init 3 objects textMessage, mobileApp and email
-2. If we add new notification class then we should new class and this cause a lot of duplicated code...
-3. In notification classes we use the same method...
-
-HOW TO FIX:
-1. Use DesignPattern Observer
- */
