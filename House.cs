4066caa25d176dfd35d1cc03d45c14cc2285b55a
Przemysław Juśkiewicz <nyweron@gmail.com>
2019-09-26 22:42:22 +0200

    Added Builder Fluent Interace

---
+++ House.cs
@@ -0,0 +1,89 @@
+namespace DesignPatterns
+{
+  class House
+  {
+    private string walls;
+    private string floor;
+    private string rooms;
+    private string roof;
+    private string windows;
+    private string doors;
+    private string garage;
+
+    public House(HouseBuilder houseBuilder)
+    {
+      walls = houseBuilder.walls;
+      floor = houseBuilder.floor;
+      rooms = houseBuilder.rooms;
+      roof = houseBuilder.roof;
+      windows = houseBuilder.windows;
+      doors = houseBuilder.doors;
+      garage = houseBuilder.garage;
+    }
+
+    public override string ToString()
+    {
+      return string.Format("[ {0}, {1}, {2}, {3}, {4}, {5}, {6}]", this.walls, this.floor, this.rooms, this.roof, this.windows, this.doors, this.garage);
+    }
+
+    public class HouseBuilder
+    {
+      public string walls;
+      public string floor;
+      public string rooms;
+      public string roof;
+      public string windows;
+      public string doors;
+      public string garage;
+
+      public HouseBuilder Walls(string walls)
+      {
+        this.walls = walls;
+        return this;
+      }
+
+      public HouseBuilder Floor(string floor)
+      {
+        this.floor = floor;
+        return this;
+      }
+
+      public HouseBuilder Rooms(string rooms)
+      {
+        this.rooms = rooms;
+        return this;
+      }
+
+      public HouseBuilder Roof(string roof)
+      {
+        this.roof = roof;
+        return this;
+      }
+
+      public HouseBuilder Windows(string windows)
+      {
+        this.windows = windows;
+        return this;
+      }
+
+      public HouseBuilder Doors(string doors)
+      {
+        this.doors = doors;
+        return this;
+      }
+
+      public HouseBuilder Garage(string garage)
+      {
+        this.garage = garage;
+        return this;
+      }
+
+      public House Build()
+      {
+        return new House(this);
+      }
+    }
+
+  }
+
+}
--- Program.cs
+++ Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using DesignPatterns.House;
 
 namespace DesignPatterns
 {
@@ -8,9 +7,9 @@ namespace DesignPatterns
     static void Main(string[] args)
     {
 
-      var house1 = new House1("walls", "floors", "rooms", "windows", "doors", "garage");
+      // var house1 = new House("walls", "floors", "rooms", "windows", "doors", "garage");
 
-      //var house2 = new House1();
+      //var house2 = new House();
       // What fields we should use now?
       // it is mess
       // what is correct order
@@ -18,6 +17,35 @@ namespace DesignPatterns
       // Builder is solving this problem
 
       Console.WriteLine("Hello World!");
+
+      // Client client = new Client.Builder()
+      //   .FirstName("Slawomir")
+      //   .LastName("Kowalski")
+      //   .ZipCode("34 174")
+      //   .MyWallet(456.32)
+      //   .HomeNumber(23)
+      //   .Town("Cambridge")
+      //   .Street("botolph lane")
+      //   .build();
+
+      // Console.WriteLine("Data client: " + "\nFirstName: " + client.FirstName +
+      //   "\nKowalski: " + client.LastName +
+      //   "\nZip code: " + client.ZipCode +
+      //   "\nWallet: " + client.MyWallet +
+      //   "\nHome number: " + client.HomeNumber +
+      //   "\nTown: " + client.Town +
+      //   "\nStreet: " + client.Street);
+
+      House houses = new House.HouseBuilder()
+        .Walls("Sciany")
+        .Doors("Drzwi")
+        .Floor("Podłoga")
+        .Build();
+
+      Console.WriteLine(houses.ToString());
+
+      Console.ReadKey();
     }
   }
+
 }
