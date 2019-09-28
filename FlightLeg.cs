1e635083692825041a662646a9a442bec78e58f5
Przemysław Juśkiewicz <nyweron@gmail.com>
2019-09-28 18:14:55 +0200

    Implemented Flight with builder as fluent interface

---
+++ FlightLeg.cs
@@ -0,0 +1,66 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace DesignPatterns
+{
+  public class FlightLeg
+  {
+    private string _from;
+    private string _to;
+    private bool _delayed;
+    private int _price;
+
+    private FlightLeg(FlightLegBuilder flightLegBuilder)
+    {
+      _from = flightLegBuilder.from;
+      _to = flightLegBuilder.to;
+      _price = flightLegBuilder.price;
+      _delayed = flightLegBuilder.delayed;
+    }
+
+    public void SetDelayed(bool delayed)
+    {
+      _delayed = delayed;
+    }
+
+    public void SetPrice(int price)
+    {
+      _price = price;
+    }
+
+    public override string ToString()
+    {
+      return $"from: {_from}\nto: {_to}\nprice: {_price}\ndelayed: {_delayed}\n";
+    }
+
+    public class FlightLegBuilder
+    {
+      public string from;
+      public string to;
+      public bool delayed;
+      public int price;
+
+      public FlightLegBuilder(string from, string to)
+      {
+        this.from = from;
+        this.to = to;
+      }
+
+      public FlightLegBuilder Price(int price)
+      {
+        this.price = price;
+        return this;
+      }
+
+      public FlightLeg Build()
+      {
+        if (this.price == 0)
+        {
+          throw new ValidationException("Required field is empty- price");
+        }
+
+        return new FlightLeg(this);
+      }
+
+    }
+  }
+}
--- Program.cs
+++ Program.cs
@@ -2,11 +2,27 @@
 
 namespace DesignPatterns
 {
+  /*
+    Class FlightLeg  should have fileds:
+    from - required, unchanged after being built by Builder
+    to - required, unchanged after being built by Builder
+    delayed - default false, can only be set via the setter
+    price- required, can be changed after creating the object
+   */
   class Program
   {
     static void Main(string[] args)
     {
-      Console.WriteLine("Hello World!");
+      FlightLeg leg = new FlightLeg.FlightLegBuilder("Las Vegas", "Los Angeles").Price(50).Build();
+      Console.WriteLine(leg);
+      leg.SetDelayed(true);
+      Console.WriteLine(leg);
+      leg.SetPrice(10);
+      Console.WriteLine(leg);
+
+      leg = new FlightLeg.FlightLegBuilder("Las Vegas", "Los Angeles").Build();
+      Console.WriteLine(leg);
+
     }
   }
 }
