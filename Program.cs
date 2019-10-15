3e4f31e7960612ad6a0a11b139d3170fffb12761
Przemysław Juśkiewicz <przemyslaw@pjuskiewicz.com>
2019-10-15 20:53:34 +0200

    Flyweight show problem

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace DesignPatterns
 {
@@ -6,7 +7,24 @@ namespace DesignPatterns
   {
     static void Main(string[] args)
     {
+      List<Object> tempList = new List<object>();
+
+      for (int i = 0; i < 10000000; i++)
+      {
+        tempList.Add(new Tank("Tank", 200, 100, 50, 25, 0, 0, 50));
+        tempList.Add(new Rifleman("Rifleman", 200, 100, 50, 25, 0, 0, 50));
+        tempList.Add(new Destroyer("Destroyer", 200, 100, 50, 25, 0, 0, 50));
+      }
+
       Console.WriteLine("Hello World!");
     }
+    /*
+    Description of problem:
+    Build many objects many times with the same properties
+    It takes a lot of memory
+
+
+    Flyweight solving problem with memory
+    */
   }
 }
